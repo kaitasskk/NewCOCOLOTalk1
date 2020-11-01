53cdc11b3382598efe3ae8fd72e9a2b9b18b46be
ShimuraKeita <keitagsk8001@gmail.com>
2020-11-01 20:39:01 +0900

    Initial Commit

---
+++ NewCOCOLOTalk1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewCOCOLOTalk1
+//
+//  Created by 志村　啓太 on 2020/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
